1b272f873f89e00db83d147398bd3267d4ef8142
mattm3192 <mattm3192@gail.com>
2025-08-26 19:29:53 -0500

    added repl.go and cleaned up main.go

--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex >")
-		scanner.Scan()
-		input := scanner.Text()
-		cleanedInput := cleanInput(input)
-		fmt.Printf("Your command was: %s\n", cleanedInput[0])
-	}
-}
-
-func cleanInput(text string) []string {
-	var words []string
-	trimString := strings.TrimSpace(text)
-	cleanedString := strings.ToLower(trimString)
-	words = strings.Split(cleanedString, " ")
-	return words
+	startRepl()
 }
