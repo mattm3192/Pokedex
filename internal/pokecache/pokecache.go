7cab953fb78db3f4d11b66dfbf5abeb93ced6771
mattm3192 <mattm3192@gail.com>
2025-08-29 02:54:00 -0500

    added pokecache internal package and relevent types and funcs/methods

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,47 @@
+package pokecache
+
+import (
+	"time"
+)
+
+func NewCache(interval time.Duration) *Cache {
+	newCache := &Cache{
+		entries:  make(map[string]cacheEntry),
+		interval: interval,
+	}
+	go newCache.reapLoop()
+	return newCache
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	createdtime := time.Now()
+	newCacheEntry := cacheEntry{createdAt: createdtime, val: val}
+	c.entries[key] = newCacheEntry
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, ok := c.entries[key]
+	if !ok {
+		return nil, false
+	}
+	return entry.val, true
+}
+
+func (c *Cache) reapLoop() {
+	ticker := time.NewTicker(c.interval)
+	defer ticker.Stop()
+	for range ticker.C {
+		c.mu.Lock()
+		for key, entry := range c.entries {
+			if time.Since(entry.createdAt) > c.interval {
+				delete(c.entries, key)
+			}
+		}
+		c.mu.Unlock()
+	}
+
+}
---
+++ internal/pokecache/types_cache.go
@@ -0,0 +1,17 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	entries  map[string]cacheEntry
+	interval time.Duration
+	mu       sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
